f339b9931584595244ed12295d65cf0a569c1d86
David-Hernan <71787287+David-Hernan@users.noreply.github.com>
2021-10-19 21:14:10 -0500

    Lab 18 terminado

--- Laboratorio18/models/muestra.js
+++ Laboratorio18/models/muestra.js
@@ -5,7 +5,7 @@ const db = require('../util/database');
     {nombre: "Vitacilina", funcion: "¡Ah qué buena medicina!"},
     {nombre: "Imodium", funcion: "Opioide contra la diarrea"}
 ];*/
-
+ 
 var indice;
 
 //El modelo es una clase
@@ -35,42 +35,15 @@ module.exports = class Muestra {
         }else{
             return db.execute('SELECT * FROM muestras WHERE nombre = ?', [id]);
         }
-        
-        /*.then(([rows, fieldData]) => {
-            //Si conexión fue exitosa, regresa las filas e infor
-            console.log(rows);
-            console.log(rows.length);
-            return rows;
-        })
-        .catch(err => {
-            //regreso e imprimo error
-            console.log(err);
-            return [];
-        });*/
-        //console.info("Conexión exitosa con la base de datos");
-        //return muestras;
     }
 
-    //-------------------------------------------------------------------
-    static modificar1(id) {
-        if (id === undefined){
-            return db.execute('SELECT * FROM muestras');
-        }else{
-            indice=id;
-            console.log(indice);
-            return db.execute('SELECT * FROM muestras WHERE indice_medicinas = ?', [indice]);
-        }
-    }
-    static modificar2() {
+    static update(nombre, funcion, indice_bodega, id) {
         return db.execute('UPDATE muestras SET nombre=? , funcion=?, indice_bodega=? WHERE indice_medicinas= ?',
-        [this.nombre, this.funcion, this.indice_bodega, indice]
+        [nombre, funcion, indice_bodega, id]
         );
     }
-    /*static modificar2() {
-        return db.execute('UPDATE muestras SET nombre="uno" , funcion="dos", indice_bodega=1 WHERE indice_medicinas= 6',
-        [indice]
-        );
-    }*/
-    //-------------------------------------------------------------------
 
+    static fetchOne(id){
+        return db.execute('SELECT * FROM muestras WHERE indice_medicinas = ?', [id]);
+    }
 }
